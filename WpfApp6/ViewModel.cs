42dfdd9124df7b207799aeb1750afe93eef808ad
sahmiran <hasancantural19@gmail.com>
2021-12-09 11:57:58 +0300

    Add project files.

---
+++ WpfApp6/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp6
+{
+    public class ViewModel
+    {
+        public ICommand MyCommand { get; set; }
+
+        public ViewModel()
+        {
+            MyCommand = new Command(ExecuteMethode , canExecuteMethode);
+        }
+
+        private bool canExecuteMethode(object parameter)
+        {
+            return true;
+        }
+
+        private void ExecuteMethode(object parameter)
+        {
+            MessageBox.Show("Command executed");
+        }
+    }
+}
